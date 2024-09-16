e2e33de9398122d805f9c1f2c3bb384a872fe956
mtbyk <af15081@shibaura-it.ac.jp>
2024-09-16 19:32:35 +0900

    fix: correct instance name casing in setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 $tarFileUrl = "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.3-x86_64.tar.gz"
 $tarFilePath = "alpineFs.tar.gz"
-$instanceName = "poshOnTmux"
+$instanceName = "PoshOnTmux"
 $installDir = "$env:USERPROFILE\tmuxOnPosh"
 $tmuxConfigPath="\\wsl$\poshOnTmux\home\tmux\.tmux.conf"
 
